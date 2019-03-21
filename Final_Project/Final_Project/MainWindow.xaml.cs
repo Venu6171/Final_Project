b4c0b6b79972cb62ce4d8b0a47a17e52331a6fdb
Viren Ahluwalia <viren_ahluwalia@yahoo.in>
2019-03-20 19:55:01 -0700

    - Created test messages for chatting that goes from down to up.

--- Final_Project/Final_Project/MainWindow.xaml.cs
+++ Final_Project/Final_Project/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Net;
 
 namespace WorseApp
 {
@@ -24,12 +25,18 @@ namespace WorseApp
     public partial class MainWindow : Window
     {
         private LocalData localAppData;
+        private ContactData contactAppData;
+        private bool AutoSize { get; set; }
 
         List<Button> contactButtons = new List<Button>();
         private int lastContactIndex = 0;
 
         List<ContactData> chats = new List<ContactData>();
 
+        List<TextBox> textBoxes = new List<TextBox>();
+        private int MessageSent = 0;
+
+        private int currentContactIndex;
 
         public LocalData LocalAppData
         {
@@ -42,8 +49,6 @@ namespace WorseApp
         }
 
         private string inputPath;
-
-
 
         private void FileButton_Click(object sender, RoutedEventArgs e)
         {
@@ -58,6 +63,7 @@ namespace WorseApp
 
         private void AddToContactsButton_Click(object sender, RoutedEventArgs e)
         {
+
             ContactWindow contactWindow = new ContactWindow();
             if (contactWindow.ShowDialog() == false)
             {
@@ -79,12 +85,15 @@ namespace WorseApp
                     contactButtons.Add(button);
                 }
 
+                IPAddressWindow iPAddressWindow = new IPAddressWindow();
+                iPAddressWindow.Show();
+
                 for (int i = lastContactIndex; i < contactButtons.Count; ++i)
                 {
                     double buttonOffset = 15.0;
                     if (i != 0)
                     {
-                        contactButtons[i].Margin = new Thickness(contactButtons[i - 1].Margin.Left, buttonOffset , contactButtons[i - 1].Margin.Right, contactButtons[i - 1].Margin.Bottom);
+                        contactButtons[i].Margin = new Thickness(contactButtons[i - 1].Margin.Left, buttonOffset, contactButtons[i - 1].Margin.Right, contactButtons[i - 1].Margin.Bottom);
                     }
                     else
                     {
@@ -103,15 +112,94 @@ namespace WorseApp
         private void ContactButtonClick(object sender, EventArgs e)
         {
             string eventSenderName = ((Button)sender).Content.ToString();
-
+            MessageInputBox.IsEnabled = true;
             CurrentContactNameLabel.Content = eventSenderName;
+            for (int i = 0; i < chats.Count; ++i)
+            {
+                if (chats[i].RecipientName == eventSenderName)
+                {
+                    currentContactIndex = i;
+                }
+            }
+            MessageStackPanel.Children.Clear();
+            ShowMessage();
         }
+
+        private void ShowMessage()
+        {
+            textBoxes.Clear();
+            if(chats[currentContactIndex].MyMessages.Count == 0)
+            {
+                return;
+            }
+            for (int i = 0; i < chats[currentContactIndex].MyMessages.Count; ++i)
+            {
+                TextBox textbox = new TextBox();
+                ContactData contactData = new ContactData();
+                textbox.Height = 28;
+                textbox.FontSize = 16;
+                textbox.FontWeight = MessageInputBox.FontWeight;
+                textbox.HorizontalAlignment = HorizontalAlignment.Right;
+                textbox.VerticalAlignment = VerticalAlignment.Bottom;
+
+                textbox.Text = chats[currentContactIndex].MyMessages[i];
+                //textbox.Margin = new Thickness(412, 326, 9, 9);
+
+                SendButton.Click += new RoutedEventHandler(SendButton_Click);
+
+                textBoxes.Add(textbox);
+            }
+
+            for (int i = 0; i < textBoxes.Count; ++i)
+            {
+                double buttonOffset = 340.0;
+
+                if (i != 0)
+                {
+                    textBoxes[i].Margin = new Thickness(textBoxes[i - 1].Margin.Left, 10, textBoxes[i - 1].Margin.Right, textBoxes[i - 1].Margin.Bottom);
+                }
+                else
+                {
+                    if (textBoxes.Count > 1)
+                    {
+                        textBoxes[i].Margin = new Thickness(textBoxes[i].Margin.Left, buttonOffset - (textBoxes.Count - 1) * 38, textBoxes[i].Margin.Right, textBoxes[i].Margin.Bottom);
+                    }
+                    else
+                    {
+                        textBoxes[i].Margin = new Thickness(textBoxes[i].Margin.Left, textBoxes[i].Margin.Top + buttonOffset, textBoxes[i].Margin.Right, textBoxes[i].Margin.Bottom);
+                    }
+                }
+            }
+
+            MessageStackPanel.Children.Clear();
+            for (int j = 0; j < textBoxes.Count; j++)
+            {
+                MessageStackPanel.Children.Add(textBoxes[j]);
+                textBoxes[j].IsEnabled = false;
+            }
+
+            MessageSent++;
+            MessageInputBox.Text = "";
+
+        }
+
 
         private void SendButton_Click(object sender, RoutedEventArgs e)
         {
             string messageToSend = MessageInputBox.Text;
+            if (messageToSend == "")
+            {
+                return;
+            }
 
-            
+            chats[currentContactIndex].MyMessages.Add(messageToSend);
+
+            ShowMessage();
+
+            MessageInputBox.Text = "";
         }
     }
 }
+
+
+
